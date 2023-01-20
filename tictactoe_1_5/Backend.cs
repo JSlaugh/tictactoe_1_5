6c4d695c6d4736c101699affd0e0d8b0ff30fd18
JSlaugh <93290565+JSlaugh@users.noreply.github.com>
2023-01-20 11:50:53 -0700

    Add project files.

---
+++ tictactoe_1_5/Backend.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+///  Joe Slaugh, Joe Lambert, Tanner Woodrum, Chris Isaksen
+/// </summary>
+namespace tictactoe_1_5
+{
+    class Backend
+    {
+        public void DisplayBoard(char[] array)
+        {
+            //This function displays the board :)
+            Console.WriteLine(array[0] + "|" + array[1] + "|" + array[2]);
+            Console.WriteLine("_____");
+            Console.WriteLine(array[3] + "|" + array[4] + "|" + array[5]);
+            Console.WriteLine("_____");
+            Console.WriteLine(array[6] + "|" + array[7] + "|" + array[8]);
+
+        }
+        public string TestWin(char[] array)
+        {
+            //This function is to test the win :)
+            if (array[0] == array[3] && array[3] == array[6])
+            {
+                if (array[0] == 'O')
+                {
+                    return "Player 1 wins!";
+                }
+                else
+                {
+                    return "Player 2 Wins!";
+                }
+    
+            }
+            else if (array[0] == array[1] && array[1] == array[2])
+            {
+                if (array[0] == 'O')
+                {
+                    return "Player 1 wins!";
+                }
+                else
+                {
+                    return "Player 2 Wins!";
+                }
+            }
+            else if (array[0] == array[4] && array[4] == array[8])
+            {
+                if (array[0] == 'O')
+                {
+                    return "Player 1 wins!";
+                }
+                else
+                {
+                    return "Player 2 Wins!";
+                }
+            }
+            else if (array[1] == array[4] && array[4] == array[7])
+            {
+                if (array[1] == 'O')
+                {
+                    return "Player 1 wins!";
+                }
+                else
+                {
+                    return "Player 2 Wins!";
+                }
+            }
+            else if (array[3] == array[4] && array[4] == array[5])
+            {
+                if (array[3] == 'O')
+                {
+                    return "Player 1 wins!";
+                }
+                else
+                {
+                    return "Player 2 Wins!";
+                }
+            }
+            else if (array[6] == array[4] && array[4] == array[2])
+            {
+                if (array[6] == 'O')
+                {
+                    return "Player 1 wins!";
+                }
+                else
+                {
+                    return "Player 2 Wins!";
+                }
+            }
+            else if (array[2] == array[5] && array[5] == array[8])
+            {
+                if (array[2] == 'O')
+                {
+                    return "Player 1 wins!";
+                }
+                else
+                {
+                    return "Player 2 Wins!";
+                }
+            }
+            else if (array[6] == array[7] && array[7] == array[8])
+            {
+                if (array[6] == 'O')
+                {
+                    return "Player 1 wins!";
+                }
+                else
+                {
+                    return "Player 2 Wins!";
+                }
+            }
+            // If the game has lasted it's full length and no one has won report a tie
+            else if (array[9] == 'T')
+            {
+                return "It's a Draw";
+            }
+
+            // report nothing if there is no winner or loser
+            return "";
+        }
+    }
+}
---
+++ tictactoe_1_5/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace tictactoe_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tic-Tac-Toe!\n");
+            // The chars will either be an O or an X
+            // instanciating the array with gameboard positions
+            // last position is used to test for a draw
+            char[] gameBoard = new char[10] {'1','2','3','4','5','6','7','8','9',' '};
+
+            //variables that hold the players' choice
+            int p1; 
+            int p2;
+
+            //instanciate backend class
+            Backend b = new Backend();
+
+            //initial display of the board
+            b.DisplayBoard(gameBoard);
+            Console.WriteLine();
+
+            //creating variables
+            bool player1 = true;
+            string response = "";
+            string message = ", select a position on the board (type the corresponding number): ";
+
+            //for loop that will go through the game with the user
+            for (int i = 0; i< gameBoard.Length; i++)
+            {
+                // player 1 will be O's and player 2 will be X's
+
+                if(player1)
+                {
+                    Console.Write($"Player 1{message}");
+
+                    p1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+
+                    gameBoard[p1-1] = 'O';
+
+                    // toggle the players
+                    player1 =   false;
+                    b.DisplayBoard(gameBoard);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write($"Player 2{message}");
+
+                    p2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+
+                    gameBoard[p2-1] = 'X';
+
+                    // toggle the players
+                    player1 =   true;
+                    b.DisplayBoard(gameBoard);
+                    Console.WriteLine();
+                }
+
+                //if we have reached the max number of turns, then we will send in the position
+                if  (i  ==  8) gameBoard[9] = 'T';
+
+                //set response message to return of TestWin method
+                response = b.TestWin(gameBoard);
+
+                // if there is a response break out of for loop
+                if(response.Length > 0) break;
+            }
+
+            // before we display the winner, we will display the board one last time
+            b.DisplayBoard(gameBoard);
+            Console.WriteLine();
+            Console.WriteLine(response);
+        }
+    }
+}
